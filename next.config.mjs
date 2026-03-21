f1d32870ed2e58bdf0c0a7d74c2e5fd2074525d0
adii <adrianmiguelflores512@gmail.com>
2026-03-21 15:42:54 +0800

    fix: tailwind v4 config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // If the error persists, you can try disabling experimental turbo 
+  // features to see if it defaults to a more stable Webpack build:
+  experimental: {
+    turbo: {
+      rules: {
+        '*.css': ['postcss-loader'],
+      },
+    },
+  },
 };
 
 export default nextConfig;
